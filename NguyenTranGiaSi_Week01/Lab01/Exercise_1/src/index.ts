4637e34f870b7723ba21e0bd69210af99624f1f4
H82M19\Student <Student>
2025-08-20 07:44:54 +0700

    feat: Complete exercise 1

---
+++ NguyenTranGiaSi_Week01/Lab01/Exercise_1/src/index.ts
@@ -0,0 +1,16 @@
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  displayInfo(): void {
+    console.log(`Name: ${this.name}, Age: ${this.age}`);
+  }
+}
+
+const person1 = new Person("Gia Si", 20);
+person1.displayInfo(); 
